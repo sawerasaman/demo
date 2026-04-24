21dc2a5f2af5855db1a74d1596723f124f84f499
sawera saman <sawerasaman901278@gmail.com>
2026-04-24 21:01:10 +0500

    main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from test import sum
+from src.login import multiply
+multiply_result = multiply(3, 4)
+print("Multiplication Result:", multiply_result)
+sum=sum(1, 2)
+
+print(sum)
+
 print("hello world")
